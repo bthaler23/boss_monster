e823c2c914b3bb6139681be28b076d0f7d1b649f
Brandon Thaler <bthaler@bellsouth.net>
2017-05-18 15:44:41 -0400

    Add Torches CSS

--- game/lib/start_screen.js
+++ game/lib/start_screen.js
@@ -21,20 +21,22 @@ class StartScreen {
 
   draw(stage) {
     stage.clearRect(0, 0, stage.canvas.width, stage.canvas.height);
-    stage.font = "100px Inknut Antiqua serif";
+    stage.font = "140px Inknut Antiqua serif";
     stage.fillStyle = '#4a433b';
     stage.fillRect(0, 0, stage.canvas.width, stage.canvas.height);
-    stage.fillStyle = '#b30000';
+    stage.fillStyle = '#AB0000';
     stage.textAlign="center";
     stage.fillText("Boss Monster", stage.canvas.width/2, stage.canvas.height * 2/5);
     stage.strokeStyle = 'black';
     stage.strokeText("Boss Monster", stage.canvas.width/2, stage.canvas.height * 2/5);
     stage.font = "50px Inknut Antiqua serif";
+    stage.fillStyle = '#780000';
     stage.fillText("Press Enter to Play", stage.canvas.width/2, stage.canvas.height * 3/5);
-    stage.strokeText("Press Enter to Play", stage.canvas.width/2, stage.canvas.height * 3/5);
-    stage.fillStyle = "black";
+    // stage.strokeText("Press Enter to Play", stage.canvas.width/2, stage.canvas.height * 3/5);
     stage.font = "25px Inknut Antiqua serif";
+    stage.fillStyle = "#540000";
     stage.fillText("WASD to Move, Click To Shoot", stage.canvas.width/2, stage.canvas.height * 7/10);
+    stage.fillStyle = "#300000";
     stage.fillText("Developed by Brandon Thaler", stage.canvas.width/2, stage.canvas.height * 4/5);
     stage.textAlign="start";
   }
